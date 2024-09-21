5e4e1d2f8d6f9deca8ccc0f181e0dbc1b509aa69
rue <rhu1@luc.edu>
2024-09-21 12:03:17 -0500

    covered the basic requirements for the rover, i think?

--- src/classes/rover_mock.py
+++ src/classes/rover_mock.py
@@ -2,3 +2,30 @@ class Rover:
     def __init__(self, x: int, y: int) -> None:
         self.x: int = x
         self.y: int = y
+
+        self.directions: list = ["N", "E", "S", "W"]
+        self.position: str = self.directions[0]
+
+    def _convertCommand(self, cmd) -> tuple[int, int, str]:
+        current = self.directions[self.position]
+
+        # insane that python let's you loop backwards on a list
+        if cmd == "L":
+            self.position = self.directions.index[current - 1]
+        if cmd == "R":
+            self.position = self.directions.index[(current + 1) % 4]
+
+        if cmd == "M":
+            self.move(self, cmd)
+
+        return [self.x, self.y, self.position]
+
+    def move(self, str) -> bool:
+        if self.position == "N":
+            self.y += 1
+        elif self.position == "E":
+            self.x += 1
+        elif self.position == "S":
+            self.y -= 1
+        elif self.position == "W":
+            self.x -= 1
