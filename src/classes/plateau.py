290ed9bde5c09f391cbc05ca2c813a5c4a2a88a1
Nicholas M. Synovic <nicholas.synovic@gmail.com>
2024-09-21 16:57:21 -0500

    Move file to a different directory

--- src/classes/plateau.py
+++ src/classes/plateau.py
@@ -1,22 +1,19 @@
 class Plateau:
-
-    def __init__(self, x: int, y : int):
+    def __init__(self, x: int, y: int):
         self.width = x
         self.height = y
-        self.grid = self.create_grid() 
-    
+        self.grid = self.create_grid()
+
     def createGrid(self):
-        return [[None for _ in range(self.x_limit + 1)] for _ in range(self.y_limit + 1)]
-    
-    def checkMove(self, x: int, y:int, rover) -> bool:
+        return [
+            [None for _ in range(self.x_limit + 1)]
+            for _ in range(self.y_limit + 1)  # noqa: E501
+        ]
+
+    def checkMove(self, x: int, y: int, rover) -> bool:
         if 0 <= x <= self.width and 0 <= y <= self.height:
             coordinate_x = rover.x + x
             coordinate_y = rover.y + y
             if coordinate_x <= self.width and coordinate_y <= self.height:
                 return True
         return False
-
-
-
-    
-    
